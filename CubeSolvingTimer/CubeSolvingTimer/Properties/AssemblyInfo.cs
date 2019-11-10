98b8026c86ee31783636bdd74945e32999e5f8fb
AmineAML <amin_aml@live.fr>
2019-11-10 13:19:56 +0100

    Modified the assembly information for the product name and description with the contact email

--- CubeSolvingTimer/CubeSolvingTimer/Properties/AssemblyInfo.cs
+++ CubeSolvingTimer/CubeSolvingTimer/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CubeSolvingTimer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Contact e-mail: amin_aml@live.fr")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CubeSolvingTimer")]
